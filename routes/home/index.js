3e640d55892280433601e56b8455003130860d6f
박성현 <vlvk8195@naver.com>
2025-03-29 00:58:06 +0900

    깃 저장소 초기화

---
+++ routes/home/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const ctrl = require("./hone.ctrl");
+
+router.get("/", ctrl.home);
+
+router.get("/login", ctrl.login);
+
+module.exports = router;
+
+
+
